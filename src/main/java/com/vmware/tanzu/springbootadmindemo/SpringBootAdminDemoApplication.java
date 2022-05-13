26ae881d1b4c9353826b6717432666a47ac9ee3c
benwilcock <benwilcock@gmail.com>
2022-05-13 13:52:40 +0100

    Configured SBA as a client of itself

--- src/main/java/com/vmware/tanzu/springbootadmindemo/SpringBootAdminDemoApplication.java
+++ src/main/java/com/vmware/tanzu/springbootadmindemo/SpringBootAdminDemoApplication.java
@@ -18,11 +18,10 @@ public class SpringBootAdminDemoApplication {
 
 	@Configuration
     public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll()  
-            .and().csrf().disable();
-    }
-}
-
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests().anyRequest().permitAll()  
+				.and().csrf().disable();
+		}
+	}
 }
