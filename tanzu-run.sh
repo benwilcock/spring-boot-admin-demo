52354772b0fea3eb08b26a83575ef2e9a6f380eb
benwilcock <benwilcock@gmail.com>
2022-05-13 15:41:08 +0100

    corrected the issue with the UI not showing and added deploy script

--- tanzu-run.sh
+++ tanzu-run.sh
@@ -4,11 +4,12 @@ tanzu apps workload create spring-boot-admin-demo \
     --git-branch main \
     --type web \
     --label app.kubernetes.io/part-of=spring-boot-admin-demo \
+    --label tanzu.app.live.view=false \
     --label tanzu.app.live.view.application.name=spring-boot-admin-demo \
     --annotation autoscaling.knative.dev/minScale=1 \
     --namespace default \
     --env "NAMESPACE=default" \
-    --env "DOMAIN=192.168.50.105.nip.io" \
+    --env "DOMAIN=apps.192.168.50.105.nip.io" \
     --env "INGRESS_PORT=31080" \
     --env "JAVA_TOOL_OPTIONS=-Dmanagement.server.port=8080" \
     --env "SPRING_PROFILES_ACTIVE=tap" \
@@ -17,3 +18,5 @@ tanzu apps workload create spring-boot-admin-demo \
 tanzu apps workload tail spring-boot-admin-demo --since 10m --timestamp
 
 watch tanzu apps workload get spring-boot-admin-demo
+
+firefox http://spring-boot-admin-demo.default.apps.192.168.50.105.nip.io:31080/
