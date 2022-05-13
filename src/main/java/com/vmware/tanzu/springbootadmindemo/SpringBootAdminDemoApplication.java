8e140cb6999bee1df7c27ff10e234230434b4c35
benwilcock <benwilcock@gmail.com>
2022-05-13 11:07:35 +0100

    initial commit

---
+++ src/main/java/com/vmware/tanzu/springbootadmindemo/SpringBootAdminDemoApplication.java
@@ -0,0 +1,13 @@
+package com.vmware.tanzu.springbootadmindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vmware/tanzu/springbootadmindemo/SpringBootAdminDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmware.tanzu.springbootadmindemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAdminDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
