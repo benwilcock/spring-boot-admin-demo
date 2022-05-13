c637d777d88ef65173b5c34922497e7b22d93e74
benwilcock <benwilcock@gmail.com>
2022-05-13 11:29:13 +0100

    added annotations and security config to enable UI

--- src/main/java/com/vmware/tanzu/springbootadmindemo/SpringBootAdminDemoApplication.java
+++ src/main/java/com/vmware/tanzu/springbootadmindemo/SpringBootAdminDemoApplication.java
@@ -2,12 +2,27 @@ package com.vmware.tanzu.springbootadmindemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@EnableAdminServer
 @SpringBootApplication
 public class SpringBootAdminDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAdminDemoApplication.class, args);
 	}
+
+	@Configuration
+    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().permitAll()  
+            .and().csrf().disable();
+    }
+}
 
 }
